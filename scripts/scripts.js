37fbdd9ed022ec4b9c499d4e44ac35f0286ce3c7
Vlad <67093050+Ascilator@users.noreply.github.com>
2020-08-06 23:45:46 +0300

    Add files via upload

---
+++ scripts/scripts.js
@@ -0,0 +1,16 @@
+﻿jQuery(function () {
+
+	$('.burger_cont').click(function(){
+		$('.burger').toggleClass('_active');
+		$('.menu_list').slideToggle(500);
+	});
+	function tabs () {
+		$(".tab_item").not(":first").hide();
+		$(".wrapper .tab").click(function() {
+			$(".tab").removeClass("_active").eq($(this).index()).addClass("_active");
+			$(".tab_item").hide().eq($(this).index()).fadeIn()
+		}).eq(0).addClass("_active");
+	}
+	tabs();
+
+});
